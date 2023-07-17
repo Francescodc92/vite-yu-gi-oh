8ec4fc33dfa43d6ad003e40b348ee287b1abebef
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-17 14:38:54 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+  import HeaderComponet from './components/HeaderComponets/HeaderComponet.vue';
+  import MainComponet from './components/MainComponets/MainComponet.vue';
+  import FooterComponet from './components/FooterComponets/FooterComponet.vue';
+  export default{
+    components:{
+      HeaderComponet,
+      MainComponet,
+      FooterComponet
+    }
+  }
+</script>
+
+<template>
+  <HeaderComponet/>
+  <MainComponet/>
+  <FooterComponet/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/MainComponets/MainComponet.vue
@@ -0,0 +1,7 @@
+<script></script>
+<template>
+ <p>main</p>
+</template>
+<style scoped>
+
+</style>
