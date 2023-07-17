ddfd396bed8f2818fe12c0b0fcbbfc4b5dfabcaf
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-17 15:20:54 +0200

    end header section

--- src/components/HeaderComponets/HeaderComponet.vue
+++ src/components/HeaderComponets/HeaderComponet.vue
@@ -1,7 +1,32 @@
-<script></script>
-<template>
- <p>header</p>
-</template>
-<style scoped>
+<script>
+export default{
+  
+}
+</script>
 
+<template>
+ <header>
+  <div class="img-wrapper">
+    <img src="../../assets/img/logo.png" alt="logo yu-gi-ho">
+  </div>
+  <span>Yu-Gi-oh Api</span>
+ </header>
+</template>
+
+
+<style lang="scss" scoped>
+header{
+  padding:.9375rem;
+  display: flex;
+  align-items: center;
+  gap:5px;
+  background-color: #F6F6F6;
+  .img-wrapper{
+    width: 70px;
+  }
+  span{
+    font-size:1.875rem;
+    font-weight: 500;
+  }
+}
 </style>
